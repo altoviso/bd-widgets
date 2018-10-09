a2cd69f9f970e7031bb81daba060c16e43dbfdaf
Rawld Gill <rgill@altoviso.com>
2018-10-09 11:11:58 -0700

    dev: add dialog widget

--- src/dialog/dialog-test.es6.js
+++ src/dialog/dialog-test.es6.js
@@ -1,13 +1,14 @@
 import {e, TestContainer, render, smoke, assert, assertClassNameEq} from "../../test/lib.js";
 import Button from "../button/Button.js";
 import Input from "../input/Input.js";
+import Labeled from "../labeled/Labeled.js";
 import Dialog from "./Dialog.js";
 
 let action = smoke.Action.action;
 let keys = action.keys;
 let top = 0;
 
-export default class RenameDialog extends Dialog {
+class RenameDialog extends Dialog {
 	constructor(kwargs){
 		super(kwargs);
 		if(!kwargs.title){
@@ -34,6 +35,32 @@ export default class RenameDialog extends Dialog {
 	}
 }
 
+
+class RenameDialog2 extends Dialog {
+	constructor(kwargs){
+		super(kwargs);
+		if(!kwargs.title){
+			this.title = "Rename";
+		}
+	}
+	dialogBody(){
+		return e("div", {className: "rename-dialog"},
+			e("div",
+				e(Labeled, {label:"Current Name"},
+					e(Input, {value: this.kwargs.currentName, style:"min-width:20em", disabled:true})
+				),
+				e(Labeled, {label:"New Name"},
+					e(Input, {bdAttach: "newName", value: this.kwargs.currentName, placeholder: "enter new name", style:"min-width:20em"})
+				),
+			),
+			e("div", {className: "bottom-buttons"},
+				e(Button, {label: "Cancel", handler: ()=> this.onCancel()}),
+				e(Button, {label: "OK", handler: ()=> this.promise.resolve(this.newName.value)})
+			)
+		);
+	}
+}
+
 smoke.defTest({
 	id: "dialog-demo",
 	before(){
@@ -50,7 +77,7 @@ smoke.defTest({
 		top.insChild(e(Button, {
 			label: "Rename",
 			tabIndex: tabIndex++,
-			handler: () => RenameDialog.show({currentName:"thisIsTheCurrentName"}).then(result => console.log("the dialog returned:", result))
+			handler: () => RenameDialog2.show({currentName:"thisIsTheCurrentName"}).then(result => console.log("the dialog returned:", result))
 		}));
 		top.insChild(e(Button, {
 			label: "End Test",
