b67ce2194759490275271487ec515c50153025a3
Rawld Gill <rgill@altoviso.com>
2018-10-09 11:10:40 -0700

    dev: add Labeled widget

---
+++ src/labeled/Labeled.js
@@ -0,0 +1,10 @@
+import {Component, e} from "../lib.js";
+
+export default class Labeled extends Component.withWatchables("label") {
+	bdElements(){
+		return e("div", {bdAttach: "bdChildrenAttachPoint"},
+			e("label", {bdReflect: "label"})
+		);
+	}
+}
+Labeled.className = "bd-labeled";
