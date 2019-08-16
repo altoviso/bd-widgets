0d3732c514eb666c94976d3e4ae64a9a41b089fb
rcgill <rgill@altoviso.com>
2019-08-16 06:41:18 -0700

    rfe: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "sourceType": "module"
+
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    "rules": {
-		"indent": "off",
-        "no-undef":["warn", { "typeof": false }],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    },
-	"overrides": [
-		{
-			"files": ["smoke.js"],
-			"rules": {
-				"quotes": "off",
-				"indent": "off"
-			},
-			"globals": {
-				"define": true
-			}
-		}
-	]
+
+    extends: [
+        'backdraft'
+    ],
+
+    'rules': {
+    }//,
+
+	// "overrides": [
+	// 	{
+	// 		"files": ["smoke.js"],
+	// 		"rules": {
+	// 			"quotes": "off",
+	// 			"indent": "off"
+	// 		},
+	// 		"globals": {
+	// 			"define": true
+	// 		}
+	// 	}
+	// ]
 };
