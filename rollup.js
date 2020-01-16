dd8c41b1916077deebef130f5e6cb897d0dfd118
rcgill <rgill@altoviso.com>
2020-01-16 11:21:54 -0800

    rfe: code cleaning with eslint

--- rollup.js
+++ rollup.js
@@ -1,8 +1,8 @@
-export {default as Button} from "./src/button/Button.js";
-export {default as ComboBox} from "./src/comboBox/ComboBox.js";
-export {default as Dialog} from "./src/dialog/Dialog.js";
-export {default as Input} from "./src/input/Input.js";
-export {default as Labeled} from "./src/labeled/Labeled.js";
-export {default as ListBox} from "./src/listBox/ListBox.js";
-export {default as Meta} from "./src/meta/Meta.js";
-export {default as StateButton} from "./src/stateButton/StateButton.js";
+export {default as Button} from './src/button/Button.js';
+export {default as ComboBox} from './src/comboBox/ComboBox.js';
+export {default as Dialog} from './src/dialog/Dialog.js';
+export {default as Input} from './src/input/Input.js';
+export {default as Labeled} from './src/labeled/Labeled.js';
+export {default as ListBox} from './src/listBox/ListBox.js';
+export {default as Meta} from './src/meta/Meta.js';
+export {default as StateButton} from './src/stateButton/StateButton.js';
